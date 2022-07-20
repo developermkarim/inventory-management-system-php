1f7cf8ace543a46256830d0502200ea3165cf9db
developermkarim <m.karimcu@gmail.com>
2022-07-20 22:27:23 +0600

    Register and login page done

--- index.php
+++ index.php
@@ -1,17 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inventory Management system</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet">
-
-</head>
-<body>
+<?php
+include('header.php');
+?>
 <div class="overlay"><div class="loader"></div></div>
 	<!-- Navbar -->
 	
@@ -41,5 +30,6 @@
 		</div>
 	</div>
 
-</body>
-</html>
+<?php
+include('footer.php');
+?>
---
+++ register.php
@@ -0,0 +1,51 @@
+<?php
+include('header.php');
+?>
+<div class="overlay"><div class="loader"></div></div>
+	<?php include('./public_html/templates/header.php'); ?>
+	<div class="container">
+		<div class="card mx-auto" style="width: 30rem;">
+	        <div class="card-header">Register</div>
+		      <div class="card-body">
+		        <form id="register_form" onsubmit="return false" autocomplete="off">
+		          <div class="form-group">
+		            <label for="username">Full Name</label>
+		            <input type="text" name="username" class="form-control" id="username" placeholder="Enter Username">
+		            <small id="u_error" class="form-text text-muted"></small>
+		          </div>
+		          <div class="form-group">
+		            <label for="email">Email address</label>
+		            <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
+		            <small id="e_error" class="form-text text-muted">We'll never share your email with anyone else.</small>
+		          </div>
+		          <div class="form-group">
+		            <label for="password1">Password</label>
+		            <input type="password" name="password1" class="form-control"  id="password1" placeholder="Password">
+		            <small id="p1_error" class="form-text text-muted"></small>
+		          </div>
+		          <div class="form-group">
+		            <label for="password2">Re-enter Password</label>
+		            <input type="password" name="password2" class="form-control"  id="password2" placeholder="Password">
+		            <small id="p2_error" class="form-text text-muted"></small>
+		          </div>
+		          <div class="form-group">
+		            <label for="usertype">Usertype</label>
+		            <select name="usertype" class="form-control" id="usertype">
+		              <option value="">Choose User Type</option>
+		              <option value="Admin">Admin</option>
+		              <option value="Other">Other</option>
+		            </select>
+		            <small id="t_error" class="form-text text-muted"></small>
+		          </div>
+		          <button type="submit" name="user_register" class="btn btn-primary"><span class="fa fa-user"></span>&nbsp;Register</button>
+		          <span><a href="index.php">Login</a></span>
+		        </form>
+		      </div>
+	      <div class="card-footer text-muted">
+	        
+	      </div>
+	    </div>
+	</div>
+<?php
+include('footer.php')
+?>
