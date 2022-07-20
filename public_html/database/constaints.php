8ef15a05ec8a6533777ccd38cdede717beb906b8
developermkarim <m.karimcu@gmail.com>
2022-07-20 21:38:36 +0600

    Readme File & database files setup

---
+++ public_html/database/constaints.php
@@ -0,0 +1,7 @@
+<?php
+define("HOST","localhost");
+define("USER","root");
+define("PASSWORD","");
+define("DB","invent_mng_system");
+define("DOMAIN","http://localhost/PROJECTS_FOR_JOB/MY-INVENTORY-MANAGEMENT-SYSTEM/public_html/");
+?>
---
+++ public_html/database/db.php
@@ -0,0 +1,17 @@
+<?php
+include_once('constaints.php');
+class Database{
+private $conn;
+public function connect()
+{
+    // $this->conn = null;
+  $this->conn = new mysqli(HOST,USER,PASSWORD,DB);
+  IF($this->conn){
+    return $this->conn;
+  }
+ return "DATABASE_CONNECTION_FAIL";
+}
+
+
+}
+?>
