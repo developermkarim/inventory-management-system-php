293a18fde84085b00ffb2d8a9ed7541199fd8870
developermkarim <m.karimcu@gmail.com>
2022-07-20 20:49:44 +0600

    Basic page segmented and form setup

---
+++ public_html/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inventory Management system</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+<body>
+<div class="overlay"><div class="loader"></div></div>
+	<!-- Navbar -->
+	
+	<br/><br/>
+	<div class="container">
+		
+		<div class="card mx-auto" style="width: 20rem;">
+		  <img class="card-img-top mx-auto" style="width:60%;" src="./images/login.png" alt="Login Icon">
+		  <div class="card-body">
+			  <!-- this form cant be submit directly as it has onsubmit return false -->
+		    <form id="form_login" onsubmit="return false">
+			  <div class="form-group">
+			    <label for="exampleInputEmail1">Email address</label>
+			    <input type="email" class="form-control" name="log_email" id="log_email" placeholder="Enter email">
+			    <small id="e_error" class="form-text text-muted">We'll never share your email with anyone else.</small>
+			  </div>
+			  <div class="form-group">
+			    <label for="exampleInputPassword1">Password</label>
+			    <input type="password" class="form-control" name="log_password" id="log_password" placeholder="Password">
+			  	<small id="p_error" class="form-text text-muted"></small>
+			  </div>
+			  <button type="submit" class="btn btn-primary"><i class="fa fa-lock">&nbsp;</i>Login</button>
+			  <span><a href="register.php">Register</a></span>
+			</form>
+		  </div>
+		  <div class="card-footer"><a href="#">Forget Password ?</a></div>
+		</div>
+	</div>
+
+</body>
+</html>
